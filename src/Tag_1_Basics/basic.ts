b4c3e05239be3aea51319a21a899d145ab2ab1dd
bebzbzbz <balzerbeatrice@gmail.com>
2024-12-19 15:20:48 +0100

    TS Intro

---
+++ src/Tag_1_Basics/basic.ts
@@ -0,0 +1,106 @@
+console.log('Welcome :)');
+
+// =========== Intro Level 1_12 ===========
+console.log("Level 1_12");
+
+let dogName = "Leon";
+console.log(dogName);
+
+// dogName = 11;
+dogName = "elf";
+
+let krank = true;
+// krank = "lebensmittelvergiftung";
+krank = false;
+console.log(krank);
+
+let age = 24;
+// age = "twenty-five";
+age = 25;
+console.log(age);
+
+let seventeen;
+// seventeen ist undefinded
+seventeen = 17;
+const sum = seventeen + 44;
+console.log(sum);
+
+// =========== Intro Level 1_13 ===========
+// Addition
+let addition_operator: number = 1 + 1;
+console.log('addition: ' + addition_operator);
+
+// Subtraktion
+let subtraktion_operator: number = 2 - 1;
+console.log('subtraktion: ' + subtraktion_operator);
+
+// Multiplikation
+let multiplication_operator: number = 2 * 2;
+console.log('multiplication: ' + multiplication_operator);
+
+// Division
+let division_operator: number = 4 / 2;
+console.log('division: ' + division_operator);
+
+// Modulo: zeigt an, was der Rest sein würde.
+let modulo_operator: number = 14 % 5;
+console.log('modulus: ' + modulo_operator);
+
+// =========== Intro Level 1_8 ===========
+console.log("Level 1_8");
+
+let x = 20;
+let y = 30;
+console.log(x + y);
+console.log(y - x);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+
+let z = 10;
+let resultOne = (x * y) / z;
+console.log(resultOne);
+
+let a = 15;
+let b = 9;
+console.log(a % b);
+
+let c = 20;
+let resultTwo = (a + b) * c;
+console.log(resultTwo);
+
+console.log(++a);
+console.log(--b);
+
+let resultThree = a - b;
+console.log(resultThree);
+
+console.log(resultOne % resultTwo);
+
+// =========== Intro Level 1_16 ===========
+console.log("Level 1_16");
+
+let score: number = 5 + 5 * 10;  
+console.log('Ergebnis: ' + score);
+
+score = (5 + 5) * 10;  
+console.log('Ergebnis: ' + score);
+
+score = 0;  
+score = score + 10;  
+console.log('Ergebnis: ' + score);
+
+score += 10;  
+console.log('Ergebnis: ' + score)
+
+let zahl = 1;  
+zahl = zahl + 1;  
+zahl += 1;
+
+// Inkrement-Operator
+zahl++;  
+console.log('increment: ' + zahl);
+
+// Dekrement-Operator
+zahl--;  
+console.log('dekrement: ' + zahl);
